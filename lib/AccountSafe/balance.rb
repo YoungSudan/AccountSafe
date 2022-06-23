f6ee22ae9dbad5d82f95d6106a781c9dbad12302
YoungSudan <lochalamoicolin83@gmail.com>
2022-06-23 04:35:49 -0400

    fix lint and test

--- lib/AccountSafe/balance.rb
+++ lib/AccountSafe/balance.rb
@@ -1,26 +1,21 @@
-require 'byebug'
+# frozen_string_literal: true
 
 module AccountSafe
+  class Balance
+    attr_accessor :assets, :liab, :equ
 
-    class Balance
-        attr_accessor :assets ,:liab , :equ 
-        def initialize(assets:0, liab:0, equ:0)
-            @assets = assets
-            @liab = liab
-            @equ = equ
-        end
-
-        def assets
-            return @liab + @equ
-        end
-
-        def equity
-            return @assets - @liab
-        end 
-
-        def validate_balance! 
-            return  @assets == @liab + @equ && @equ == @assets - @liab
-        end
+    def initialize(assets: 0, liab: 0, equ: 0)
+      @assets = assets
+      @liab = liab
+      @equ = equ
     end
 
+    def equity
+      @assets - @liab
+    end
+
+    def validate_balance!
+      @assets == @liab + @equ && @equ == @assets - @liab
+    end
+  end
 end
--- lib/account_safe.rb
+++ lib/account_safe.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "AccountSafe/version"
-require_relative "AccountSafe/balance"
-require_relative "AccountSafe/income"
+require_relative 'AccountSafe/version'
+require_relative 'AccountSafe/balance'
+require_relative 'AccountSafe/income'
 # Main class for calculating slimple finacial equations
 module AccountSafe
   class Error < StandardError; end
