e7f5bd99314754e9a89faf1073efa6183ea0eb5e
YoungSudan <lochalamoicolin83@gmail.com>
2022-06-23 02:02:53 -0400

    Initial bundle gem command

---
+++ spec/AccountSafe_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe AccountSafe do
+  it "has a version number" do
+    expect(AccountSafe::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
